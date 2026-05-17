9e89e8482fa49865c8ec3ce010ff57069ec96e64
jtrajano <jlt212000@yahoo.com>
2026-05-17 16:32:01 +0800

    setup shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,26 @@
 import { useEffect, useState} from 'react'
 import './App.css'
+import { Button } from './components/ui/button';
 
 function App() {
   const [message, setMessage] = useState('');
-  useEffect(() => {
-    fetch('api/hello')
-    .then(res => res.text())
-    .then(data => setMessage(data))
-  }, []);
+  // useEffect(() => {
+  //   fetch('api/hello')
+  //   .then(res => res.text())
+  //   .then(data => setMessage(data))
+  // }, []);
+
+  async function handleClick() {
+    const response = await fetch('/api/hello')
+    const data = await response.text()
+    setMessage(data)
+  }
 
   return (
    <div>
     <p className={`font-bold p-4 text-3xl`}>{message}</p>
+
+    <Button onClick={handleClick}>Destructive</Button>
     </div>
   )
 }
